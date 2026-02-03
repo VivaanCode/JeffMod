bab7c62f5ec973753ba306e3d5b7a0b49e624059
Vivaan <87912804+VivaanCode@users.noreply.github.com>
2026-02-03 15:50:54 -0800

    pls work pls psl pls

---
+++ src/main/java/org/example/jeffmod/mixin/LivingEntityMixin.java
@@ -0,0 +1,28 @@
+package org.example.jeffmod.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.registry.entry.RegistryEntry;
+import org.example.jeffmod.effect.ModEffects;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin {
+    @Shadow
+    public abstract boolean hasStatusEffect(RegistryEntry<StatusEffect> effect);
+
+    @Inject(method = "addStatusEffect(Lnet/minecraft/entity/effect/StatusEffectInstance;)Z", at = @At("HEAD"), cancellable = true)
+    private void doPurified(StatusEffectInstance effect, CallbackInfoReturnable<Boolean> cir) {
+        if (this.hasStatusEffect(ModEffects.PURIFIED_ENTRY)) {
+            if (!effect.getEffectType().value().isBeneficial()) {
+                cir.setReturnValue(false);
+            }
+        }
+    }
+
+}
